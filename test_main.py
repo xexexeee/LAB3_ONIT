0fcdc9a69f21b0d5a0ed968a8494741f067cb876
Vladimir <osipovvova2016@yandex.ru>
2026-03-11 17:14:34 +0300

    Ошибка 300 ci

--- test_main.py
+++ test_main.py
@@ -7,5 +7,5 @@ client = TestClient(app)
 def test_login_page():
     # Проверяем, что страница логина доступна (код 200)
     response = client.get("/login")
-    assert response.status_code == 200
+    assert response.status_code == 300
     assert "Вход" in response.text
